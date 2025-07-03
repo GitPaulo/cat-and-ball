80d53fb164b3b45077e9dc6a7d97fb3fdfa27011
Paulo Santos <GitPaulo@users.noreply.github.com>
2025-07-03 00:32:31 +0000

    new headers to stop caching

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ fastify.get("/", (req, reply) => {
   const startSend = debug ? process.hrtime.bigint() : 0;
   reply
     .header("Content-Type", "image/svg+xml")
-    .header("Content-Encoding", "gzip")
+    .header("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
     .send(buffer)
     .then(() => {
       if (debug) {
